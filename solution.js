9cf02703d139abc5adf8f7f46c717d45ccc5076d
Tucker Triggs <tucker.triggs@yahoo.com>
2017-05-24 14:05:01 -0600

    made bcrypt work

--- solution.js
+++ solution.js
@@ -1,9 +1,13 @@
-function hash(plaintext) {
+var bcrypt = require("bcrypt");
 
+function hash(plaintext) {
+  var encrypted = bcrypt.hashSync(plaintext, 8);
+  return encrypted;
 }
 
 function compare(plaintext, hash) {
-
+  var decoded = bcrypt.compareSync(plaintext, hash);
+  return decoded;
 }
 
 module.exports = {
